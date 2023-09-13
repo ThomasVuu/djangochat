35b37cb70c805ac5900f07ea6a06b8e500adcbf7
Thomas Vu <thomas.vu@hotmail.com>
2023-09-13 19:57:42 +0200

    joining a chat

---
+++ room/consumers.py
@@ -0,0 +1,26 @@
+import json
+
+from channels.generic.websocket import AsyncWebsocketConsumer
+from asgiref.sync import sync_to_async
+
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = f'chat_{self.room_name}'
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+        await self.accept()
+        await self.send(text_data=json.dumps({
+            'message': f'You are now in {self.room_name}'
+        }))
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
+        await self.send(text_data=json.dumps({
+            'message': f'You have left {self.room_name}'
+        }))
